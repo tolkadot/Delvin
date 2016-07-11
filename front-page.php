8c97c14550de64497d54cafa17506aed9932efea
tolkadot <tolkadot@gmail.com>
2016-07-11 23:48:26 +0000

    pop-up contact

--- front-page.php
+++ front-page.php
@@ -31,11 +31,50 @@ function frontPageHeading() {
 
 }
 add_action( 'genesis_after_header', 'frontPageHeading' );
+?>
 
+<nav>
+<ul>
+ 	<li><a href="home.html">home</a></li>
+ 	<li><a href="portfolio.html">portfolio</a></li>
+ 	<li><a href="#contact">contact </a></li>
+ 	<li><a href="#about">about </a></li>
+</ul>
+</nav><section id="home">
+<h1 id="banner">tolka</h1>
+<h2>web design</h2>
+</section>
 
+<section id="blurb">
+<div class="blurbText floatLeft"> 
+I love to help people with their web presence.  
+</br>
+Together we will sit down and work out what you would like your website to achieve, who your target audience is and how we can best pitch your business to the world.
+</br> 
+Specialising in building customised Wordpress themes to suit your unique needs. I also have experience with Big Cartel and Shopify customisation.   </div> 
+<div class="blurbText floatLeft"> 
+Are you a graphic designer with more work than time. 
+</br>
+With skills in HTML5, CSS, Javascript and PHP I can help you out.  I am comfortable building Wordpress themes or working with existing ones. 
+</br> 
+Specialising in the Genesis Framework incorporating Compass and SCSS into my web development processes. </div> 
+ </section>
 
+<section id="connect">
+<div class="centerDiv"><a href="#spu-61" role="button" data-toggle="modal"><h3> Contact Me </h3></a></div>
+</section>
 
+<section id="about">
+<h3 id="aboutMe">About Me </h3>
+<div class="col col30 bgblue">hello</div>
+<div class="col col40 bggold">hello</div>
+<div class="col col30 bgblack">hello</div>
+<div class="col col30 bggreen">hello</div>
+<div class="col col30 bgpink">hello</div>
+<div class="col col40 bgred">hello</div>
+</section>
 
+<?php
 genesis();
 
 
--- js/aboutScreenSize.js
+++ js/aboutScreenSize.js
@@ -2,11 +2,15 @@ jQuery(function( $ ){
     
 function screen(){
 var $w = $(window).width()*0.44; 
+var $w1 = $(window).width()*0.5;
+var $w3 = $(window).width();
 var $w2 = $(window).width()*0.28; 
 var $h = $("#about").height()/2; 
 $('.col').css({'height': $h +'px'});
 $('.col40').css({'width': $w +'px'});
 $('.col30').css({'width': $w2 +'px'});
+$('.col50').css({'width': $w1 +'px'});
+$('.col100').css({'width': $w3 +'px'});
 console.log($w);
 }
 
